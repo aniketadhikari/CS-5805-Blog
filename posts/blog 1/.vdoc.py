529d6da417427d90ff825ed5798b457b23a62971
Aniket Adhikari <67086347+aniketadhikari@users.noreply.github.com>
2023-11-14 09:19:01 -0500

    Blog Post 1

---
+++ posts/blog 1/.vdoc.py
@@ -0,0 +1,90 @@
+# type: ignore
+# flake8: noqa
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+import sys
+
+assert sys.version_info >= (3, 7)
+#
+#
+#
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+np.random.seed(42)
+
+plt.rc('font', size=12)
+plt.rc('axes', labelsize=14, titlesize=14)
+plt.rc('legend', fontsize=12)
+plt.rc('xtick', labelsize=10)
+plt.rc('ytick', labelsize=10)
+#
+#
+#
+#
+
+#
+#
+#
+#
+
+#
+#
+#
